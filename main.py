bfe3d15d110049151f6d4959ed1a9592a06a3547
Кубарева Елизавета Петровна <e.kubareva@emk.loc>
2022-07-06 19:42:25 +0300

    my first comit

---
+++ main.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+s=Service("C:\\Users\\e.kubareva\\PycharmProjects\\pythonProject\\drivers\\windows\\chromedriver.exe")
+browser = webdriver.Chrome(service=s)
+urls = ["https://fit.fitcoach4everyday.com/", "https://easysplits.fitcoach4everyday.com/","https://meditation.fitcoach4everyday.com/"]
+
+try:
+    for url in urls:
+        print(url)
+        browser.get(url=url)
+        time.sleep(5)
+except Exception as error:
+    print(error)
+finally:
+    browser.close()
+    browser.quit()
