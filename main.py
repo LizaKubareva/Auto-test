34e5c4cb5cd195103d7c3aac08ba06dc463c451e
e.kubareva <kubarebaliza8@gmail.com>
2022-07-08 12:40:21 +0300

    change1

--- main.py
+++ main.py
@@ -3,16 +3,21 @@ from selenium.webdriver.chrome.service import Service
 import time
 s=Service("C:\\Users\\e.kubareva\\PycharmProjects\\pythonProject\\drivers\\windows\\chromedriver.exe")
 browser = webdriver.Chrome(service=s)
-urls = ["https://fit.fitcoach4everyday.com/", "https://easysplits.fitcoach4everyday.com/","https://meditation.fitcoach4everyday.com/"]
+urls = ["https://myfitnessclub.ru/", "http://24fitnesshome.com", "https://dreambodyathome.com", "http://onlyfamilyplan.com "]
+
 
 try:
     for url in urls:
         print(url)
-        browser.get(url=url)
-        time.sleep(5)
-        print("Лиза коцык")
+        page = browser.get (url=url)
+        time.sleep(4)
+        btn_support = page.find_element_by_class('btn_support')
+        btn_support.click()
+        time.sleep(4)
 except Exception as error:
     print(error)
 finally:
     browser.close()
     browser.quit()
+
+
